4f770828b5b0e3211c675088abe8e3a113aa9f5e
NGUYEN XUAN HUY p1419771 <xuan-huy.nguyen@etu.univ-lyon1.fr>
2018-04-17 22:40:04 +0200

    Server: 'joi' for validation request - Client: add vuetify, show error if exist

--- server/src/controller/AuthenticationController.js
+++ server/src/controller/AuthenticationController.js
@@ -3,11 +3,8 @@ const User = require('../models/User');
 module.exports = {
     async register (req, res){
         try{
-            //const user = await User.create(req.body);
-            //res.send(JSON.stringify(user));
-            User.findById(6).then(e => {
-                console.log(JSON.stringify(e));
-            })
+            const user = await User.create(req.body);
+            res.send(JSON.stringify(user));
         } catch (err) {
             res.status(400).send({
                 error: 'This email account is already in use.'
--- server/src/routes/register.js
+++ server/src/routes/register.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const AuthenticationController = require('../controller/AuthenticationController')
+const AuthenticationController = require('../controller/AuthenticationController');
+const AuthenticationControllerPolicy = require('../policies/AuthenticationControllerPolicy');
 
-router.post('/', AuthenticationController.register);
+router.post('/', AuthenticationControllerPolicy.register, AuthenticationController.register);
 
 module.exports = router;
